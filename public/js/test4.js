3a7e2d86743a10c7cad5e6449aae40e9b3a930ed
tresch <tresch@gmail.com>
2016-12-31 11:34:40 -0500

    split controllers

---
+++ public/js/test4.js
@@ -0,0 +1,48 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller("poolController", function ($scope,$http) {
+
+	    $scope.poolData = '10.0';
+
+    	    $http.get('/pool').
+        	success(function(data) {
+	    		console.log('success', data);
+            		$scope.data = data;
+            		$scope.poolData = data;
+		}).error(function(data, status) {
+	    		console.log('error', data, status);
+            		$scope.data = 'error with status code: ' + status;
+        	});
+});
+
+myApp.controller("garageController", function ($scope,$http) {
+
+            $scope.garageData = '0.0';
+
+            $http.get('/garage').
+                success(function(data) {
+                        console.log('success', data);
+                        $scope.data = data;
+                        $scope.garageData = data;
+                }).error(function(data, status) {
+                        console.log('error', data, status);
+                        $scope.data = 'error with status code: ' + status;
+                });
+});
+
+myApp.controller("bleController", function ($scope,$http) {
+
+            $scope.bleData = '0.0';
+
+            $http.get('/ble').
+                success(function(data) {
+                        console.log('success', data);
+                        $scope.data = data;
+                        $scope.bleData = data;
+                }).error(function(data, status) {
+                        console.log('error', data, status);
+                        $scope.data = 'error with status code: ' + status;
+                });
+
+	});
+
