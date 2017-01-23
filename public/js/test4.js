bff965bc451e28f6e7170733d37e598db2b6d06e
tresch <tresch@gmail.com>
2017-01-23 00:59:37 -0500

    fix interval to have data immediately

--- public/js/test4.js
+++ public/js/test4.js
@@ -6,6 +6,16 @@ myApp.controller("poolController", function ($scope,$http,$interval) {
 
 	    $scope.poolData = '10.0';
 
+            $http.get('/pool').
+                success(function(data) {
+                        console.log('success', data);
+                        $scope.data = data;
+                        $scope.poolData = data;
+                }).error(function(data, status) {
+                        console.log('error', data, status);
+                        $scope.data = 'error with status code: ' + status;
+                });
+
 	    $poolTimer = $interval(function() {
     	    $http.get('/pool').
         	success(function(data) {
@@ -22,6 +32,16 @@ myApp.controller("poolController", function ($scope,$http,$interval) {
 myApp.controller("tvRoomController", function ($scope,$http,$interval) {
 
             $scope.tvRoomData = '10.0';
+
+	    $http.get('/tvroom').
+                success(function(data) {
+                        console.log('success', data);
+                        $scope.data = data;
+                        $scope.tvRoomData = data;
+                }).error(function(data, status) {
+                        console.log('error', data, status);
+                        $scope.data = 'error with status code: ' + status;
+                });
 
             $tvRoomTimer = $interval(function() {
             $http.get('/tvroom').
@@ -41,6 +61,16 @@ myApp.controller("garageController", function ($scope,$http,$interval) {
 
             $scope.garageData = '0.0';
 
+            $http.get('/garage').
+                success(function(data) {
+                        console.log('success', data);
+                        $scope.data = data;
+                        $scope.garageData = data;
+                }).error(function(data, status) {
+                        console.log('error', data, status);
+                        $scope.data = 'error with status code: ' + status;
+                });
+
             $garageTimer = $interval(function() {
             $http.get('/garage').
                 success(function(data) {
@@ -57,6 +87,16 @@ myApp.controller("garageController", function ($scope,$http,$interval) {
 myApp.controller("bleController", function ($scope,$http,$interval) {
 
             $scope.bleData = '0.0';
+
+            $http.get('/ble').
+                success(function(data) {
+                        console.log('success', data);
+                        $scope.data = data;
+                        $scope.bleData = data;
+                }).error(function(data, status) {
+                        console.log('error', data, status);
+                        $scope.data = 'error with status code: ' + status;
+                });
 
             $bleTimer = $interval(function() {
             $http.get('/ble').
